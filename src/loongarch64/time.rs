a6fea28c1f6e70702c42c33a291c1afcc09386b9
zhang252112914 <2010286495@qq.com>
2025-04-09 20:22:45 +0800

    add time in loon

---
+++ src/loongarch64/time.rs
@@ -0,0 +1,35 @@
+use core::time::Duration;
+use loognArch64::register::time;  // like riscv, we should put it in the third-party dir
+use config::{board::clock_freq, time::INTERRUPTS_PER_SECOND};
+
+
+pub fn get_time() -> usize {
+    time::read()
+}
+
+pub fn get_time_ms() -> usize {
+    time::read() / (clock_freq() / 1_000)
+}
+
+pub fn get_time_sec() -> usize {
+    time::read() / clock_freq()
+}
+
+/// microseconds 微秒
+pub fn get_time_us() -> usize {
+    time::read() / (clock_freq() / 1_000_000)
+}
+
+pub fn get_time_duration() -> Duration {
+    Duration::from_micros(get_time_us() as u64)
+}
+
+pub unsafe fn set_next_timer_irq() {
+    let next_trigger: u64 = (time::read() + clock_freq() / INTERRUPTS_PER_SECOND) as u64;
+    sbi_rt::set_timer(next_trigger);
+}
+
+pub unsafe fn set_timer_irq(times: usize) {
+    let next_trigger: u64 = (time::read() + times * clock_freq() / INTERRUPTS_PER_SECOND) as u64;
+    sbi_rt::set_timer(next_trigger);
+}
